9c1cb9cd9ddf398a14c43e166acb57e6d42d2ee7
betanzos515 <betanzos515@gmail.com>
2018-10-17 17:02:45 -0500

    Update Numero.js

---
+++ Numero.js
@@ -0,0 +1,24 @@
+const numero1 = 30,
+      numero2 = "20",
+      numero3 = 20.20,
+      numero4 = -3,
+      numero5 = 20;
+
+
+    console.log(numero1);
+    console.log(numero2);
+
+    let resultado;
+    console.log(resultado);
+    //sumas 
+
+    resultado = numero1 + numero5;
+
+    //resta
+    resultado = numero1-numero5;
+    //multiplicar
+    resultado = numero1* numero5;
+    resultado = numero1/numero5;
+
+    console.log(resultado);
+    
