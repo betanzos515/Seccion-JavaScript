92c5cc9888d0714d8b6c99c693b638af103952b1
betanzos515 <betanzos515@gmail.com>
2018-10-18 16:48:11 -0500

    Segundo

--- Numero.js
+++ Numero.js
@@ -21,4 +21,54 @@ const numero1 = 30,
     resultado = numero1/numero5;
 
     console.log(resultado);
-    
+
+    let dato;
+    dato =Number("20");
+    dato = Number("-10.20");
+    dato = Number(true);
+    dato = Number(false);
+    dato = Number(null);
+    dato = Number("Hola Mundo");
+    dato = Number([1,2,3,4]);
+
+
+    dato = parseFloat(10.54);
+    dato = parseInt(10.54);
+
+    console.log(dato);
+
+    //toFixed nos permite elegir el numero
+    //de decimales que manejaremos en una sifra
+
+    let num1 = "10120180.1021082812";
+    let num2 = 10294294859238.2385820203948;
+
+    //estamos convirtiendo a valor numerico y eligiendo el numero de decimales que se tomarán en cuenta.
+    console.log(Number(num1).toFixed(2));
+    console.log(num2.toFixed(10));
+
+    let dato1 = 98475638472;
+    let year = 1985;
+
+    console.log(String(dato1));
+    console.log(String(year));
+
+    //objetos en javaScript
+
+    //Object Literal 
+    const persona = {
+        nombre:'juan',
+        apellido:'De la Torre',
+        edad:80,
+        trabajo:true,
+        musica:['Trance','Rock','Grunge'],
+        hogar : {
+            ciudad:'Oaxaca',
+            pais:'Mexico'
+        }
+    }
+
+    console.log(persona.nombre + " "+ persona.trabajo+" "+persona.hogar.pais);
+    console.log(persona);
+
+    console.log(persona['nombre']+persona['hogar']['pais']);
